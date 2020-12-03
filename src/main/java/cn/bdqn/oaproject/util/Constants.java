7dbbba897708613f7e6f0083d83892c368e56ca2
Administrator <17862688047@163.com>
2020-12-03 13:48:58 +0800

    提交

--- src/main/java/cn/bdqn/oaproject/util/Constants.java
+++ src/main/java/cn/bdqn/oaproject/util/Constants.java
@@ -3,6 +3,6 @@ package cn.bdqn.oaproject.util;
 public class Constants {
 	public final static String USER_SESSION = "userSession";
 	public final static String SYS_MESSAGE = "message";
-	public final static int pageSize = 2;
+	public final static int pageSize = 5;
 	public  final static String JI="ji";
 }
